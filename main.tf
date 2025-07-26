a7bf652189831e24263a8d5f69a9c4158e6cfd85
Aditya529p <adityapatra890@gmail.com>
2025-07-26 23:35:00 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,15 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "your-artifact-bucket-name"
-}
-
-resource "aws_iam_role" "codepipeline_role" {
-  name = "codepipeline-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Principal = {
-        Service = "codepipeline.amazonaws.com"
-      },
-      Effect = "Allow",
-      Sid    = ""
-    }]
-  })
-}
-
 resource "aws_codepipeline" "my_pipeline" {
-  name     = "MyCodePipeline"
+  name     = "my-pipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
+
   artifact_store {
-    location = aws_s3_bucket.codepipeline_bucket.bucket
+    location = aws_s3_bucket.artifact_store.bucket
     type     = "S3"
   }
 
   stage {
     name = "Source"
+
     action {
       name             = "Source"
       category         = "Source"
@@ -38,6 +17,7 @@ resource "aws_codepipeline" "my_pipeline" {
       provider         = "GitHub"
       version          = "1"
       output_artifacts = ["source_output"]
+
       configuration = {
         Owner      = "your-github-username"
         Repo       = "your-repo-name"
@@ -48,16 +28,36 @@ resource "aws_codepipeline" "my_pipeline" {
   }
 
   stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.my_project.name
+      }
+    }
+  }
+
+  stage {
     name = "Deploy"
+
     action {
       name            = "Deploy"
       category        = "Deploy"
       owner           = "AWS"
       provider        = "CodeDeploy"
-      input_artifacts = ["source_output"]
+      input_artifacts = ["build_output"]
       version         = "1"
+
       configuration = {
-        ApplicationName = aws_codedeploy_app.my_app.name
+        ApplicationName     = aws_codedeploy_app.my_app.name
         DeploymentGroupName = aws_codedeploy_deployment_group.my_group.deployment_group_name
       }
     }
