d57d4829a0037b7d7212b592a06d8584ba5641c4
Aditya529p <adityapatra890@gmail.com>
2025-07-27 00:20:54 +0530

    codedeploy.tf

--- codedeploy.tf
+++ codedeploy.tf
@@ -1,15 +1,21 @@
 resource "aws_codedeploy_app" "my_app" {
-  name = "MyCodeDeployApp-new"
+  name             = "MyCodeDeployApp-${random_string.suffix.result}"  # Unique name
   compute_platform = "Server"
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  upper   = false
+  special = false
 }
 
 resource "aws_codedeploy_deployment_group" "my_group" {
   app_name              = aws_codedeploy_app.my_app.name
-  deployment_group_name = "MyDeploymentGroup"
+  deployment_group_name = "MyDeploymentGroup-${random_string.suffix.result}"
   service_role_arn      = aws_iam_role.codepipeline_role.arn
 
   deployment_style {
-    deployment_type = "IN_PLACE"
+    deployment_type  = "IN_PLACE"
     deployment_option = "WITHOUT_TRAFFIC_CONTROL"
   }
 }
