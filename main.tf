bfc9cb9a0f8b4b0904fca0fbb4711597ffecaf8c
Aditya529p <adityapatra890@gmail.com>
2025-07-26 23:40:25 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,133 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "github_token" {
+  description = "GitHub OAuth token"
+  type        = string
+  sensitive   = true
+}
+
+resource "aws_s3_bucket" "artifact_store" {
+  bucket = "my-artifact-bucket-unique-name-1234" # change to a unique bucket name
+}
+
+# IAM role for CodePipeline
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy_attach" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+}
+
+# IAM role for CodeBuild
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+# IAM role for CodeDeploy
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_policy_attach" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
+}
+
+# CodeBuild project
+resource "aws_codebuild_project" "my_project" {
+  name          = "my-codebuild-project"
+  description   = "Build project for CodePipeline"
+  build_timeout = 5
+
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+# CodeDeploy application
+resource "aws_codedeploy_app" "my_app" {
+  name = "my-codedeploy-app"
+  compute_platform = "Server"
+}
+
+# Deployment group (for EC2 or in-place)
+resource "aws_codedeploy_deployment_group" "my_group" {
+  app_name              = aws_codedeploy_app.my_app.name
+  deployment_group_name = "my-deploy-group"
+  service_role_arn      = aws_iam_role.codedeploy_role.arn
+
+  deployment_style {
+    deployment_type = "IN_PLACE"
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+  }
+
+  # NOTE: You must register EC2 instances to this group using tags or Auto Scaling
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Name"
+      value = "MyEC2Instance"
+      type  = "KEY_AND_VALUE"
+    }
+  }
+}
+
+# CodePipeline
 resource "aws_codepipeline" "my_pipeline" {
   name     = "my-pipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
