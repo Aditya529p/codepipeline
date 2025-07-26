f0a460ad4e71b683882b3d84abd1c4cbcfab65a7
Aditya529p <adityapatra890@gmail.com>
2025-07-27 00:50:34 +0530

    codedeploy.tf

--- codedeploy.tf
+++ codedeploy.tf
@@ -1,5 +1,5 @@
 resource "aws_codedeploy_app" "my_app" {
-  name             = "MyCodeDeployApp-${random_string.suffix.result}"  # Unique name
+  name             = "MyCodeDeployApp6666222"
   compute_platform = "Server"
 }
 
