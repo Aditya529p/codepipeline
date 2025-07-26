a4e950fcfc0b74a23af62e8679c3473d36961584
Aditya529p <adityapatra890@gmail.com>
2025-07-27 01:31:05 +0530

    iam.tf

--- iam.tf
+++ iam.tf
@@ -1,24 +1,23 @@
-resource "aws_iam_role" "codebuild_service_role" {
-  name = "codebuild-service-role"
+resource "aws_iam_role" "code_deploy_role" {
+  name = "codep-role-aditya8515555"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
+          Service = "codedeploy.amazonaws.com"
+        },
         Action = "sts:AssumeRole"
       }
     ]
   })
 
   tags = {
-    Name = "CodeBuildServiceRole"
+    Name = "CodeDeployRole"
   }
 }
-
 resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   role       = aws_iam_role.codebuild_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
