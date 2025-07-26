fbd24a5a6904858c6a3c6b0b7cfdf95473bc0479
Aditya529p <adityapatra890@gmail.com>
2025-07-26 23:40:57 +0530

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "codepipeline_name" {
   description = "The name of the CodePipeline"
 }
 
-output "s3_bucket_name" {
-  value       = aws_s3_bucket.artifact_store.bucket
-  description = "The name of the S3 bucket used for storing pipeline artifacts"
-}
-
 output "codebuild_project_name" {
   value       = aws_codebuild_project.my_project.name
   description = "The name of the CodeBuild project"
@@ -18,7 +13,12 @@ output "codedeploy_app_name" {
   description = "The name of the CodeDeploy application"
 }
 
-output "codedeploy_deployment_group" {
-  value       = aws_codedeploy_deployment_group.my_deployment_group.deployment_group_name
+output "deployment_group_name" {
+  value       = aws_codedeploy_deployment_group.my_group.deployment_group_name
   description = "The name of the CodeDeploy deployment group"
+}
+
+output "artifact_bucket_name" {
+  value       = aws_s3_bucket.artifact_store.bucket
+  description = "The artifact S3 bucket used by CodePipeline"
 }
